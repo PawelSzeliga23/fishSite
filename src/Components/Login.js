55141691216226e8b6ecf63923ad8ca7e0dd7531
s27030 <s27030@pjwstk.edu.pl>
2025-03-02 22:54:59 +0100

    auth start

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {Auth} from '../Config/firebase';
+import {createUserWithEmailAndPassword} from 'firebase/auth';
 
 const Login = () => {
     return (
         <div>
-            <h4>Hello World</h4>
+            <h4>Login</h4>
+            <form>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" />
+                </div>
+                <button type="submit">Sign In</button>
+            </form>
         </div>
     );
 };
